a9acb5976fdcf6a967c8bbe7b3407840569610de
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-07-07 03:45:22 +0200

    experimental slide generation

--- src/pptx-gen.ts
+++ src/pptx-gen.ts
@@ -1,16 +1,12 @@
-import { readdir, readFile } from "node:fs/promises";
-import { join } from "node:path";
 import pptxgen from "pptxgenjs";
+import { join } from "node:path";
+import { mkdir, readdir, readFile, writeFile } from "node:fs/promises";
 import { downloadsPathCOA, downloadsPathBackgrounds, formatFileName } from "./wiki-scraper";
 import { log, LogStyle } from "./logger";
 import type { City, Map, Voivodeship } from "./types";
-
-async function readFileAsB64(path: string) {
-    const file = await readFile(path);
-    const content = file.toString("base64");
-
-    return content;
-}
+import sharp from "sharp";
+import TextToSVG, { type GenerationOptions } from "text-to-svg"
+import { existsSync } from "node:fs";
 
 function findCoatOfArms(files: string[], city: City) {
     const fileName = files.find(x => x.startsWith(formatFileName(city)));
@@ -21,8 +17,26 @@ function findCoatOfArms(files: string[], city: City) {
     return join(downloadsPathCOA, fileName);
 }
 
+function chunk<T>(array: T[], size: number): T[][] {
+    const arr = [];
+    for (let i = 0; i < array.length; i += size)
+        arr.push(array.slice(i, i + size));
+    return arr;
+}
+
+const slidesPath = join(import.meta.dirname, "..", "data", "slides");
+
 export async function generatePresentation(voivodeships: Map<Voivodeship>) {
-    log([LogStyle.blue], "PRESGEN", "Generating pptx");
+    if (!existsSync(slidesPath)) {
+        log([LogStyle.yellow], "WARN", `Slides directory "${slidesPath}" doesn't exist, creating one for you`);
+        await mkdir(slidesPath).catch(_ => {
+            throw new Error(`Error creating directory ${slidesPath}`);
+        });
+    }
+
+    log([LogStyle.purple], "VERBOSE", "Loading TextToSVG");
+    const textToSVG = TextToSVG.loadSync();
+
     log([LogStyle.purple], "VERBOSE", `Reading "${downloadsPathCOA}" for coats of arms`);
 
     let coaFiles: string[] = [];
@@ -33,120 +47,229 @@ export async function generatePresentation(voivodeships: Map<Voivodeship>) {
         log([LogStyle.red, LogStyle.bold], "ERROR", `Couldn't read downloads directory for existing files: ${err}`);
     }
 
-    const presentation = new pptxgen();
-    presentation.layout = "LAYOUT_16x9";
-
-    const titleSlide = presentation.addSlide();
-    titleSlide.addText("Wszystkie miasta Polski", {
-        align: "center",
-        valign: "middle",
-        x: 0,
-        y: 0,
-        h: "100%",
-        w: "100%",
-        fontSize: 36,
-        fontFace: "Work Sans",
-        bold: true,
-    });
-
-    const citiesPerPage = 5;
-    const bottomMargin = 0.05; // gap between pages
-
-    const leftPadding = 0.3; // space from left
-    const blockHeight = 5.625 / citiesPerPage - (bottomMargin * ((citiesPerPage - 1) / citiesPerPage)); // 5.625 inches height of presentation - 1.125 when 5 per page
-
-    const herbHeight = blockHeight - 0.25;
-    const herbWidth = herbHeight / 1.6;
+    log([LogStyle.blue], "PRESGEN", "Generating slides");
 
     for (const voivodeshipName of Object.keys(voivodeships)) {
-        let currentSlide: pptxgen.Slide;
+        const chunks = chunk(voivodeships[voivodeshipName], 5);
+        const slides = [];
 
-        const voivodeshipTitleSlide = presentation.addSlide();
-        voivodeshipTitleSlide.background = { color: "#000000" };
-        voivodeshipTitleSlide.addText(voivodeshipName, {
-            align: "center",
-            valign: "middle",
-            x: 0,
-            y: 0,
-            h: "100%",
-            w: "100%",
-            fontSize: 36,
-            fontFace: "Work Sans",
-            color: "#ffffff",
-            bold: true,
-        });
-
-        for (const [i, city] of voivodeships[voivodeshipName].entries()) {
-            if (i % 5 == 0) currentSlide = presentation.addSlide();
-
-            currentSlide!.background = { color: "#2e2e2e" };
-
-            const yPos = (blockHeight + bottomMargin) * (i % citiesPerPage);
-            const backgroundImage = await readFileAsB64(
-                join(downloadsPathBackgrounds, formatFileName(city, ".edited.webp"))
-            ).catch((err) => {
-                log(
-                    [LogStyle.red, LogStyle.bold],
-                    "PPTX ERR",
-                    `Couldn't read background image for: ${city.name}, ${err}`
-                );
+        for (const [i, chunk] of chunks.entries()) {
+            const slide = sharp({
+                create: {
+                    width: 1920,
+                    height: 1080,
+                    channels: 4,
+                    background: "#FEFEFE"
+                }
             });
 
-            if (backgroundImage)
-                currentSlide!.addImage({
-                    data: `data:image/png;base64,${backgroundImage}`,
-                    x: 0,
-                    y: yPos,
-                    h: blockHeight,
-                    w: "100%",
+            const slideComposites: sharp.OverlayOptions[] = [];
+
+            for (const [i, city] of chunk.entries()) {
+                const entryHeight = 216;
+                const yOffset = i * entryHeight;
+
+                const entry = sharp({
+                    create: {
+                        width: 1920,
+                        height: entryHeight,
+                        channels: 4,
+                        background: "#000000"
+                    }
+                });
+                const entryComposites: sharp.OverlayOptions[] = [];
+
+                // add background
+                // -------------------------------------------------------------
+                await readFile(
+                    join(downloadsPathBackgrounds, formatFileName(city, ".edited.webp"))
+                ).then(buff => {
+                    entryComposites.push({
+                        input: buff,
+                        top: 0,
+                        left: 0
+                    });
+                }).catch((err) => {
+                    log(
+                        [LogStyle.red, LogStyle.bold],
+                        "PPTX ERR",
+                        `Couldn't read background image for: ${city.name}, ${err}`
+                    );
                 });
 
-            currentSlide!.addText(city.name, {
-                valign: "middle",
-                x: leftPadding,
-                y: yPos,
-                h: blockHeight,
-                fontSize: 14,
-                color: "#ffffff",
-            });
+                // city name
+                // -------------------------------------------------------------
+                const options: GenerationOptions = {
+                    fontSize: 32,
+                    anchor: "top",
+                    attributes: { fill: "white" },
+                };
+                const metrics = textToSVG.getMetrics(city.name, options);
+                const d = textToSVG.getD(city.name, options);
+                entryComposites.push({
+                    input: Buffer.from(`
+                    <svg xmlns="http://www.w3.org/2000/svg" width="${metrics.width}" height="${metrics.height}" viewBox="0 0 ${metrics.width} ${metrics.height}">
+                        <path d="${d}" fill="white" />
+                    </svg>
+                    `.trim()),
+                    top: Math.round((216 - metrics.height) / 2),
+                    left: 100
+                });
 
-            currentSlide!.addText(city.totalPopulation.toString(), {
-                valign: "middle",
-                x: leftPadding + 1.5,
-                y: yPos,
-                h: blockHeight,
-                fontSize: 14,
-                color: "#ffffff",
-            });
+                // COA
+                // -------------------------------------------------------------
+                const coaFileName = findCoatOfArms(coaFiles, city);
+                if (!coaFileName) {
+                    log([LogStyle.yellow, LogStyle.bold], "WARN", `Skipping ${city.name} -- no COA`);
+                    continue;
+                };
 
-            const coaFileName = findCoatOfArms(coaFiles, city);
-            if (!coaFileName) continue;
+                const coaBuffer = await readFile(coaFileName).catch(() => {
+                    log(
+                        [LogStyle.bold, LogStyle.red],
+                        "FILE NOT FOUND",
+                        `No COA found for '${city.name}'. This may happen due to errors in scraping. Exiting...`
+                    );
+                    log([LogStyle.bold, LogStyle.purple], "VERBOSE", `${join(downloadsPathCOA, formatFileName(city, ".png"))}`)
+                    process.exit(1);
+                });
 
-            const herbData = await readFileAsB64(coaFileName).catch(() => {
-                log(
-                    [LogStyle.bold, LogStyle.red],
-                    "FILE NOT FOUND",
-                    `No COA found for '${city.name}'. This may happen due to errors in scraping. Exiting...`
-                );
-                log([LogStyle.bold, LogStyle.purple], "VERBOSE", `${join(downloadsPathCOA, formatFileName(city, ".png"))}`)
-                process.exit(1);
-            });
+                const resizedCOA = await sharp(coaBuffer).resize({ height: 216 }).png().toBuffer();
+                const coaMeta = await sharp(resizedCOA).metadata();
 
-            currentSlide!.addImage({
-                data: `data:image/png;base64,${herbData}`,
-                x: 10 - leftPadding - herbWidth,
-                y: yPos + (blockHeight - herbHeight) / 2,
-                h: herbHeight,
-                w: herbWidth,
-            });
+                entryComposites.push({
+                    input: resizedCOA,
+                    top: 0,
+                    left: 1920 - (coaMeta.width ?? 0)
+                });
+
+                const entryBuffer = await entry.composite(entryComposites).png().toBuffer();
+                slideComposites.push({
+                    input: entryBuffer,
+                    top: yOffset,
+                    left: 0
+                });
+            }
+
+            const slideBuffer = await slide.composite(slideComposites).png().toBuffer();
+            await writeFile(join(slidesPath, `${voivodeshipName}.${i}.png`), slideBuffer);
         }
     }
 
-    const fileName = join(import.meta.dir, "..", "data", "presentation.pptx")
-    
-    await presentation.writeFile({
-        fileName,
-    });
+    // const presentation = new pptxgen();
+    // presentation.layout = "LAYOUT_16x9";
 
-    log([LogStyle.cyan, LogStyle.italic], "PPTX", `Finished generating. Output file: ${fileName}`);
+    // const titleSlide = presentation.addSlide();
+    // titleSlide.addText("Wszystkie miasta Polski", {
+    //     align: "center",
+    //     valign: "middle",
+    //     x: 0,
+    //     y: 0,
+    //     h: "100%",
+    //     w: "100%",
+    //     fontSize: 36,
+    //     fontFace: "Work Sans",
+    //     bold: true,
+    // });
+
+    // const citiesPerPage = 5;
+    // const bottomMargin = 0.05; // gap between pages
+
+    // const leftPadding = 0.3; // space from left
+    // const blockHeight = 5.625 / citiesPerPage - (bottomMargin * ((citiesPerPage - 1) / citiesPerPage)); // 5.625 inches height of presentation - 1.125 when 5 per page
+
+    // const herbHeight = blockHeight - 0.25;
+    // const herbWidth = herbHeight / 1.6;
+
+    // for (const voivodeshipName of Object.keys(voivodeships)) {
+    //     let currentSlide: pptxgen.Slide;
+
+    //     const voivodeshipTitleSlide = presentation.addSlide();
+    //     voivodeshipTitleSlide.background = { color: "#000000" };
+    //     voivodeshipTitleSlide.addText(voivodeshipName, {
+    //         align: "center",
+    //         valign: "middle",
+    //         x: 0,
+    //         y: 0,
+    //         h: "100%",
+    //         w: "100%",
+    //         fontSize: 36,
+    //         fontFace: "Work Sans",
+    //         color: "#ffffff",
+    //         bold: true,
+    //     });
+
+    //     for (const [i, city] of voivodeships[voivodeshipName].entries()) {
+    //         if (i % 5 == 0) currentSlide = presentation.addSlide();
+
+    //         currentSlide!.background = { color: "#2e2e2e" };
+
+    //         const yPos = (blockHeight + bottomMargin) * (i % citiesPerPage);
+    //         const backgroundImage = await readFileAsB64(
+    //             join(downloadsPathBackgrounds, formatFileName(city, ".edited.webp"))
+    //         ).catch((err) => {
+    //             log(
+    //                 [LogStyle.red, LogStyle.bold],
+    //                 "PPTX ERR",
+    //                 `Couldn't read background image for: ${city.name}, ${err}`
+    //             );
+    //         });
+
+    //         if (backgroundImage)
+    //             currentSlide!.addImage({
+    //                 data: `data:image/png;base64,${backgroundImage}`,
+    //                 x: 0,
+    //                 y: yPos,
+    //                 h: blockHeight,
+    //                 w: "100%",
+    //             });
+
+    //         currentSlide!.addText(city.name, {
+    //             valign: "middle",
+    //             x: leftPadding,
+    //             y: yPos,
+    //             h: blockHeight,
+    //             fontSize: 14,
+    //             color: "#ffffff",
+    //         });
+
+    //         currentSlide!.addText(city.totalPopulation.toString(), {
+    //             valign: "middle",
+    //             x: leftPadding + 1.5,
+    //             y: yPos,
+    //             h: blockHeight,
+    //             fontSize: 14,
+    //             color: "#ffffff",
+    //         });
+
+    //         const coaFileName = findCoatOfArms(coaFiles, city);
+    //         if (!coaFileName) continue;
+
+    //         const herbData = await readFileAsB64(coaFileName).catch(() => {
+    //             log(
+    //                 [LogStyle.bold, LogStyle.red],
+    //                 "FILE NOT FOUND",
+    //                 `No COA found for '${city.name}'. This may happen due to errors in scraping. Exiting...`
+    //             );
+    //             log([LogStyle.bold, LogStyle.purple], "VERBOSE", `${join(downloadsPathCOA, formatFileName(city, ".png"))}`)
+    //             process.exit(1);
+    //         });
+
+    //         currentSlide!.addImage({
+    //             data: `data:image/png;base64,${herbData}`,
+    //             x: 10 - leftPadding - herbWidth,
+    //             y: yPos + (blockHeight - herbHeight) / 2,
+    //             h: herbHeight,
+    //             w: herbWidth,
+    //         });
+    //     }
+    // }
+
+    // const fileName = join(import.meta.dir, "..", "data", "presentation.pptx")
+    
+    // await presentation.writeFile({
+    //     fileName,
+    // });
+
+    // log([LogStyle.cyan, LogStyle.italic], "PPTX", `Finished generating. Output file: ${fileName}`);
 }
