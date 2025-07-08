af6b75ea7a5cb2dabf9e0a3fffedd693927e1f61
feiMopps <132297930+feiMopps@users.noreply.github.com>
2025-07-08 20:01:02 +0200

    Create logger, add `tokio` as a dependency

---
+++ src/logger.rs
@@ -0,0 +1,41 @@
+#[derive(Clone, Copy)]
+pub enum LogStyle {
+    Bold = 1,
+    Italic = 3,
+    Red = 31,
+    Green = 32,
+    Yellow = 33,
+    Blue = 34,
+    Purple = 35,
+    Cyan = 36,
+    Grey = 90,
+}
+
+impl std::fmt::Display for LogStyle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "\x1b[{}m", *self as usize)
+    }
+}
+
+impl From<LogStyle> for String {
+    fn from(value: LogStyle) -> Self {
+        format!("\x1b[{}m", value as usize)
+    }
+}
+
+pub(crate) fn log_msg<const N: usize>(colors: [LogStyle; N], prefix: &str, message: String) {
+    println!(
+        "[{}{prefix:15}] {message}",
+        colors
+            .iter()
+            .map(|x| Into::<String>::into(*x))
+            .collect::<String>()
+    );
+}
+
+#[macro_export]
+macro_rules! log {
+    ($colors:expr, $prefix:tt, $($arg:tt)*) => {
+        log_msg($colors, $prefix, format!("{}", format_args!($($arg)*)))
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use logger::LogStyle;
+use logger::log_msg;
+
+mod logger;
+
+#[tokio::main]
+async fn main() {
+    log!([LogStyle::Bold, LogStyle::Italic], "DUPA", "{}", "aeiou");
+    
     println!("Hello, world!");
 }
