c16f1f513c7b3385c3266b4d1c403b3b68197970
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-07-08 19:07:52 +0200

    Hello world!

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const slideWidth = 1920;
-export const slideHeight = 1080;
-export const citiesPerSlide = 4;
-export const cityWidth = slideWidth;
-export const cityHeight = slideHeight / citiesPerSlide;
-export const cityPadding = 16;
