fef15acf2228700f6fcf044355f76d5367848a5f
feiMopps <132297930+feiMopps@users.noreply.github.com>
2025-07-13 21:15:00 +0200

    add `AppError` and `AppResult` wrapper These can be used to join various error types.

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,42 @@
-use std::{fs, path::Path};
-
 use crate::parser::City;
+use std::{fs, io, path::Path};
 
-pub fn ensure_exists(path: &Path) -> std::io::Result<()> {
+pub type AppResult<T> = Result<T, AppError>;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum AppError {
+    Io(io::Error),
+    Request(reqwest::Error),
+    Other(String),
+}
+
+impl From<io::Error> for AppError {
+    fn from(value: io::Error) -> Self {
+        AppError::Io(value)
+    }
+}
+
+impl From<reqwest::Error> for AppError {
+    fn from(value: reqwest::Error) -> Self {
+        AppError::Request(value)
+    }
+}
+
+impl From<String> for AppError {
+    fn from(value: String) -> Self {
+        AppError::Other(value)
+    }
+}
+
+impl std::error::Error for AppError {}
+impl std::fmt::Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+pub fn ensure_exists(path: &Path) -> AppResult<()> {
     if !path.exists() {
         fs::create_dir_all(path)?;
     }
@@ -10,9 +44,9 @@ pub fn ensure_exists(path: &Path) -> std::io::Result<()> {
 }
 
 pub fn format_file_name(city: &City) -> String {
-    format!(
-        "{}+{}",
-        city.identifier,
-        city.name.replace(" ", "_")
-    )
+    format!("{}+{}", city.identifier, city.name.replace(" ", "_"))
+}
+
+pub fn format_file_name_parts(city_identifier: &str, city_name: &str) -> String {
+    format!("{}+{}", city_identifier, city_name.replace(" ", "_"))
 }
