a1eccdc975e9b92a8a943e3cb9a47714295324f0
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-09-18 13:18:50 +0200

    (WIP) some basic tokenization

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,7 @@
+#[derive(Debug)]
 enum Token<'s> {
-    Identifier(&'s str),
-    Plus,
+    Special(&'s str), // #? statements
+    String(&'s str),
 }
 
 struct Lexer<'s> {
@@ -9,12 +10,76 @@ struct Lexer<'s> {
 }
 
 impl<'s> Lexer<'s> {
-    fn new(input: &'s str) -> Lexer<'s> {
+    fn new(input: &'s str) -> Self {
         Lexer { input, pos: 0 }
     }
 
-    fn next_token(&mut self) -> Option<Token<'s>> {
-        todo!();
+    fn peek_char(&self) -> Option<char> {
+        self.input[self.pos..].chars().next()
+    }
+
+    fn read_char(&mut self) -> Option<char> {
+        let c = self.peek_char()?;
+        self.pos += c.len_utf8();
+        Some(c)
+    }
+
+    pub fn next_token(&mut self) -> Option<Token<'s>> {
+        while let Some(chr) = self.read_char() {
+            match chr {
+                '#' => match self.peek_char() {
+                    Some('?') => {
+                        let start = self.pos - chr.len_utf8();
+                        self.read_char(); // consume '?'
+
+                        while let Some(next) = self.peek_char() {
+                            if next == '\n' {
+                                break;
+                            }
+                            self.read_char();
+                        }
+
+                        let slice = &self.input[start..self.pos];
+                        return Some(Token::Special(slice));
+                    }
+                    _ => {
+                        let line_start =
+                            self.pos == 1 || self.input[..self.pos - 1].ends_with('\n');
+                        if line_start {
+                            while let Some(next) = self.peek_char() {
+                                if next == '\n' {
+                                    break;
+                                }
+                                self.read_char();
+                            }
+                            continue;
+                        } else {
+                            let start = self.pos - chr.len_utf8();
+                            while let Some(next) = self.peek_char() {
+                                if next == '\n' {
+                                    break;
+                                }
+                                self.read_char();
+                            }
+                            let slice = &self.input[start..self.pos];
+                            return Some(Token::String(slice));
+                        }
+                    }
+                },
+                '\n' => continue,
+                _ => {
+                    let start = self.pos - chr.len_utf8();
+                    while let Some(next) = self.peek_char() {
+                        if next == '\n' {
+                            break;
+                        }
+                        self.read_char();
+                    }
+                    let slice = &self.input[start..self.pos];
+                    return Some(Token::String(slice));
+                }
+            }
+        }
+        None
     }
 }
-
