da3a2401358c9f6c8c2138012139d14895693689
Przemysław Szafraniec <39964594+Nimplex@users.noreply.github.com>
2025-09-22 22:44:14 +0200

    skip already transparent

--- data/voivodeships/remove-background.ps1
+++ data/voivodeships/remove-background.ps1
@@ -8,7 +8,14 @@ $fuzz = "10%"  # or whatever fuzz you need
 
 Get-ChildItem -Filter *.png | ForEach-Object {
     $file = $_.FullName
-    $target = [System.IO.Path]::Combine($_.DirectoryName, ($_.BaseName + "_transparent.png"))
+    $baseName = $_.BaseName
+
+    if ($baseName -like "*_transparent*") {
+        Write-Host "Skipping $file (already transparent)"
+        return
+    }
+
+    $target = [System.IO.Path]::Combine($_.DirectoryName, ($baseName + "_transparent.png"))
 
     if (Test-Path $target) {
         Write-Host "Skipping $target (already exists)"
